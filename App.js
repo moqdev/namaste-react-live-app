1d6dd5b20bef758b86640939ccdf46291dacb00d
Mona Waseem <monaqwaseem@gmail.com>
2023-03-10 14:21:20 -0500

    Add content to README.md, Created Header, Title and Nav-items Component.

--- App.js
+++ App.js
@@ -21,40 +21,75 @@ import ReactDOM, { createRoot } from "react-dom/client";
      - copyrights
   */
 
-// Build the layout.
-// Step 1. Create your AppLayout, list it out. 
-
+// Step 1. Create your AppLayout, list it out.
 const AppLayout = () => {
-  return (
-    { 
-      /**
-    Header
-      - Logo
-      - Nav Items (Right Side)
-      - Cart
-    Body 
-     - Search Bar
-      - List of Restaurants
-       - Restaurant Card
-        - Image
-        - Name
-        - Rating
-        - Price
-        - Cusines
-    Footer
-     - links
-     - copyrights
-  */ 
+  return {};
+};
 
-    }
-  );
+// Step 2. Build the Header and Header Components.
+// What are the components in the Header? Title/Logo, NavItems, Cart..
+// Step 2.a. Create the Title Component.
+// Step 2.b. Create the NavItems Component.
+
+const Title = () => (
+  <h1 id="title" key="h2">
+    HANGRY
+  </h1>
+);
 
 const HeaderComponent = () => {
   return (
-    <div>
-      <Title />
-      <h2>Namaste React - Most commonly Used Syntax</h2>
-    </div>
+    <section className="showcase">
+      <div className="header">
+        <Title />
+        <div className="toggle"></div>
+      </div>
+      <div className="overlay"></div>
+      <div className="text">
+        <h2>Cure Your Hunger </h2>
+        <h3>
+          All Your Favorite Restaurants and more, delivered right at your door
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod.{" "}
+        </p>
+        <a href="#">Enter Delivery Address</a>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About US</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Cart</a>
+          </li>
+        </ul>
+      </div>
+      <ul className="social">
+        <li>
+          <a href="#">
+            <img src="https://i.ibb.co/x7P24fL/facebook.png"></img>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="https://i.ibb.co/Wnxq2Nq/twitter.png"></img>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="https://i.ibb.co/ySwtH4B/instagram.png"></img>
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 };
 
---
+++ README.md
@@ -0,0 +1,5 @@
+# Mo's Namaste React Live Project, PLUS Tips & Tricks!
+
+## Summary
+
+This repository contains all of the notes taken from [Namaste React Course by NamasteDev.com](https://learn.namastedev.com/).
