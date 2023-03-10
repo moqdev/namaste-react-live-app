e70513f21e1f9598441ba7d3523e11fa62722790
Mona Waseem <monaqwaseem@gmail.com>
2023-03-10 14:18:02 -0500

    Created App Layout.

--- App.js
+++ App.js
@@ -1,27 +1,54 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 
+// APP LAYOUT
+/**
+    Header
+      - Title/Logo
+      - Nav Items (Right Side)
+      - Cart
+    Body 
+     - Search Bar
+      - List of Restaurants
+       - Restaurant Card
+        - Image
+        - Name
+        - Rating
+        - Price
+        - Cusines
+    Footer
+     - links
+     - copyrights
+  */
 
-// // React Element Example C: Third Way
-const Title = () => (
-  <h1 id="title" key="h2">
-    Food Villa App
-  </h1>
-);
+// Build the layout.
+// Step 1. Create your AppLayout, list it out. 
 
-// Example C: You can use it just like an opening and closing tag
-const HeaderComponentC = () => {
+const AppLayout = () => {
   return (
-    <div>
-      <Title></Title>
-      <h2>Namaste React - Third Example</h2>
-      <h2>This is a h2 tag</h2>
-    </div>
-  );
-};
+    { 
+      /**
+    Header
+      - Logo
+      - Nav Items (Right Side)
+      - Cart
+    Body 
+     - Search Bar
+      - List of Restaurants
+       - Restaurant Card
+        - Image
+        - Name
+        - Rating
+        - Price
+        - Cusines
+    Footer
+     - links
+     - copyrights
+  */ 
 
-// Most commonly used Santax:
-// Is the self closing tag syntax --> <Title />
+    }
+  );
+
 const HeaderComponent = () => {
   return (
     <div>
