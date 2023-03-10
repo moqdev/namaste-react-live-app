6fcbd1edf7819cdb8e1bc77588192e091b2178aa
Mona Waseem <monaqwaseem@gmail.com>
2023-03-10 14:14:17 -0500

    Add most commonly used syntax

--- App.js
+++ App.js
@@ -1,79 +1,36 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 
-// CAN YOU JUST USE REACT IN YOUR FOOTER ONLY? YES! --> By your "root".
-// Change the root to "footer" and it will only render in the footer.
-// AND In index.html change the id="root" to id="footer"
 
-// React Element Example A: (varible --> title must be lowercase)
-const title = (
+// // React Element Example C: Third Way
+const Title = () => (
   <h1 id="title" key="h2">
-    Food Villa
+    Food Villa App
   </h1>
 );
 
-//Must use curly braces and title must be lowercase
+// Example C: You can use it just like an opening and closing tag
+const HeaderComponentC = () => {
+  return (
+    <div>
+      <Title></Title>
+      <h2>Namaste React - Third Example</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
+
+// Most commonly used Santax:
+// Is the self closing tag syntax --> <Title />
 const HeaderComponent = () => {
   return (
     <div>
-      {title}
-      <h2>Namaste React</h2>
-      <h2>This is a h2 tag</h2>
-    </div>
-  );
-};
-
-// Functional Component Example B: (varible --> Title must be capatalize)
-// to render using tag: root.render(<HeaderComponent />);
-const Title = () => (
-  <h1 id="title" key="h2">
-    Namaste React
-  </h1>
-);
-
-// 2 ways:
-// 1.B: no curly braces, --> Title must be capatalize & <Title />> must be in < /tag> format
-// 2.B: you can call Title as a function {Title()}
-//Example 1.B: <Title />> must be in < /tag> format:
-const HeaderComponent1 = () => {
-  return (
-    <div>
       <Title />
-      <h2>Namaste React Functional Component with Title Tag 🤪</h2>
-      <h2>This is a h2 tag</h2>
-    </div>
-  );
-};
-
-//2.B: you can call Title as a function {Title()}
-const HeaderComponent2 = () => {
-  return (
-    <div>
-      {Title(console.log("Hello World"))}
-      <h2>Namaste React Functional Component with Title function 🤪</h2>
-      <h2>This is a h2 tag</h2>
-    </div>
-  );
-};
-
-// WRITING COMMENTS INSIDE JSX?
-
-const HeaderComponent3 = () => {
-  return (
-    <div>
-      {
-        // This is a comment.
-        /**
-         *
-         *
-         * This is a multi line comment.
-         *
-         */
-      }
+      <h2>Namaste React - Most commonly Used Syntax</h2>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeaderComponent2 />);
+root.render(<HeaderComponent />);
