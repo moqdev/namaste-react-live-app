a7bd28f27e83cc598bb5d121b4020006531f8115
Mona Waseem <monaqwaseem@gmail.com>
2023-03-12 21:53:34 -0400

    Implementing React Fragment in our code.

--- App.js
+++ App.js
@@ -21,25 +21,41 @@ import ReactDOM, { createRoot } from "react-dom/client";
      - copyrights
   */
 
-// Step 1. Create your AppLayout, list it out.
-const AppLayout = () => {
-  return {};
+const Body = () => {
+  return (
+    <h4>Body</h4>
+  );
 };
 
-// Step 2. Build the Header and Header Components.
-// What are the components in the Header? Title/Logo, NavItems, Cart..
-// Step 2.a. Create the Title Component.
-// Step 2.b. Create the NavItems Component.
+const Footer = () => {
+  return (
+    <h4>Footer</h4>
+  );
+};
+
+
+const AppLayout = () => {
+  return (
+    <Header/>
+    <Body/>
+    <Footer/>
+
+  );
+};
 
 const Title = () => (
-  <h1 id="title" key="h2">
-    HANGRY
-  </h1>
+  <a href="/">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://imgs.search.brave.com/AhuyhPSZ1YUTp8_Pmag_PT73JbMQBCPF2dkZeCsmCQY/rs:fit:400:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5h/YjVaR0FMNFd2VTdW/Y3ZxMmN4YUdnQUFB/QSZwaWQ9QXBp"
+    />
+  </a>
 );
 
 const HeaderComponent = () => {
   return (
-    <div>
+    <div className="header">
       <Title />
       <div className="nav-items">
         <ul>
@@ -52,6 +68,36 @@ const HeaderComponent = () => {
     </div>
   );
 };
+
+// Important to know about JSX: 
+// JSX can only have one parent element.
+// If you want to have multiple elements, you can wrap them in a div or use React.Fragment
+// Better to use React.Fragment because it doesn't add extra divs to the DOM.
+// React.Fragment is coming from React library (import React from "react";).
+// React.Fragment is like an EMPTY TAG. 
+// React.Fragment is a component, it doesn't render anything. It's used to wrap multiple components. 
+// It's used to avoid adding extra divs to the DOM!
+const App = () => {
+  return (
+    <React.Fragment>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+// But we can also use <> </> instead of React.Fragment - looks cleaner.
+const App = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
