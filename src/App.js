87d7aaf203e60394d8084a32c25409e09e438b0a
Mona Waseem <monaqwaseem@gmail.com>
2023-03-16 10:35:49 -0400

    Add components folder and create Title.js and export default Title.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Title from "./components/Title";
 
 // APP LAYOUT
 /**
@@ -20,16 +21,6 @@ import ReactDOM from "react-dom/client";
      - links
      - copyrights
   */
-
-const Title = () => (
-  <a href="/">
-    <img
-      className="logo"
-      alt="logo"
-      src="https://imgs.search.brave.com/AhuyhPSZ1YUTp8_Pmag_PT73JbMQBCPF2dkZeCsmCQY/rs:fit:400:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5h/YjVaR0FMNFd2VTdW/Y3ZxMmN4YUdnQUFB/QSZwaWQ9QXBp"
-    />
-  </a>
-);
 
 const Header = () => {
   return (
