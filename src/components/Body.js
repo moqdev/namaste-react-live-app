222b65f9ad80cef5d15dc41263393e237391b5c8
Mona Waseem <monaqwaseem@gmail.com>
2023-03-20 14:32:01 -0400

    Breaking down state, hooks and useState in our code.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,17 @@
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+//What is state?  State is used to store data about the component, that can change over time. It's similar to props.
+
+// What is hook? Hooks are functions that let you “hook into” React state, Hooks don’t work inside classes — they let you use React without classes.
+
+// What is useState Hook? useState hook is a function that allows you to add state to functional components.
 
 const Body = () => {
   let searchTxt = "KFKFCCCCC"; // This is the search text but hardcoded for now
 
+  //searchText is a local state variable and useState is the hook function.
   const [searchText] = useState(); // Created varible with useState hook.
 
   return (
