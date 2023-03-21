5281194660e61f642897ae46dbb553eadcde48b2
Mona Waseem <monaqwaseem@gmail.com>
2023-03-20 20:20:42 -0400

    Tinkering with the state variable.

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,11 @@ import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
 const Body = () => {
-  const [searchInput, setSearchInput] = useState("");
+  //destructure the state varible (first element searchText)
+  //the first element is the varible name and the second element is the function to update the variable
+  const searchvar = useState(); // returns = [ varible, function to update the variable called state varible ] an array ,
+
+  const [searchText, setSearchText] = searchvar;
   return (
     <>
       <div className="search-container">
@@ -11,20 +15,16 @@ const Body = () => {
           type="text"
           className="search-input"
           placeholder="Search for restaurants"
-          value={searchInput}
-          onChange={(e) => {
-            setSearchInput(e.target.value);
-            // console.log(searchInput);
-          }}
+          value={searchText}
         />
-        <button className="search-btn">Search - {searchInput} </button>
-        <div className="restaurant-list">
-          {restaurantList.map((restaurant) => {
-            return (
-              <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-            );
-          })}
-        </div>
+        <button className="search-btn"> </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurantList.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
       </div>
     </>
   );
